9645acfbba19678abf762ca6c2db50b5f9662625
GLaDOS (Hermes) <glados@hermes.local>
2026-08-25 11:25:02 +0200

    style: satisfy shellcheck -S warning repo-wide
    Automated cleanup (authorized proactive sprint): quote/export config vars,
    add '|| exit' to cd, drop unused captures. Verified locally.

--- mount-usb.sh
+++ mount-usb.sh
@@ -3,7 +3,7 @@ DISCLAIMER(){
 # DISCLAIMER START
 echo ' BE EXTREMELY CAREFULL! '
 echo 'i am in no way to be held responsible for loss of data, revenue or any damage done to your system by YOU running this script WITHOUT READING IT FIRST!'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 }
@@ -13,9 +13,9 @@ CONFIG(){
 echo 'Start configuring'
 RETP_FOLDER="/home/pi/RetroPie"
 MNTPNT="/media/usb0"
-mnt="mount $MNTPNT $RET_FOLDER"
 UUID="XXXX-XXXX"
 EX4UUID="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXX"
+export EX4UUID
 }
 AUTOMATIC(){
 # Automatic Mount
@@ -33,11 +33,11 @@ echo 'NOTE if you have a large ROM collection already on the SD card it will cop
 echo 'It is easiest if you haven`t added any roms yet.'
 echo ''
 echo 'Once the folder structure is copied over the USB will be mounted over the RetroPie folder so any ROMs you add to your pi will be run off of the USB.'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 echo 'automatic method done'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 }
@@ -57,7 +57,7 @@ echo ''
 echo 'Transfer the existing RetroPie file structure'
 echo 'This step is mandatory regardless of whether you have any roms on your system. RetroPie has a specific directory structure and a number of files required packaged with even empty installations.'
 echo ''
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo "Either via SFTP, or using the terminal (via exiting emulationstation, pressing F4, or remotely using [[SSH]]), move the $RETP_FOLDER folder into your USB stick."
 echo "The reason for moving the whole folder, and not just $RETP_FOLDER/roms is that there are other folders, such as $RETP_FOLDER/BIOS' that are worth keeping on the external drive also."
@@ -69,7 +69,7 @@ echo ''
 echo "Look for an entry on $MNTPNT, or similar."
 echo ''
 echo 'The important things to note down are the mount point: $MNTPNT, and the position on the device tree: /dev/sda1'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo ''
 echo 'Now we can move our existing RetroPie folder to our new USB drive.'
@@ -80,40 +80,40 @@ echo 'Configure fstab to automatically mount USB drive'
 echo ''
 echo 'Establish the drive`s UUID number by entering the command ls -l /dev/disk/by-uuid/.'
 echo ''
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo 'Edit fstab with this command: sudo nano /etc/fstab and add a new line like the below:'
 echo "UUID=$UUID	$RETP_FOLDER	vfat	nofail,user,uid=pi,gid=pi	0	2"
 echo '...where UUID= the UUID of your drive, and everything else is the same as the example. Note that each item is tab delimited. If you use spaces instead of tabs this will not work.'
 echo ''
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $k
 echo 'In the case of errors with ext4 file systems use:'
 echo 'UUID="$EX4UUID" $RETP_FOLDER ext4 nofail,defaults 0    0'
 echo ''
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo 'In the case you want to allow execution of file with fat32 file system (E.g : OpenBOR), use:'
 echo "UUID=$UUID	$RETP_FOLDER	vfat	rw,exec,uid=pi,gid=pi,umask=022	0	2"
 echo ''
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo 'Restart system'
 echo ''
 echo 'This must be a full restart, not just emulationstation. When it boots up you should see any ROMs you previously had show up in emulationstation.'
 echo ''
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 #sudo reboot
 echo 'Transfer ROMs'
 echo 'Now transfer ROMs either directly to the USB drive, or via any of the usual methods (aside from using the automatic USB copy, obviously!).'
 echo 'Now that the USB drive is mounted directly to home/pi/RetroPie, every time this directory is accessed, you`re actually accessing the USB drive.'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 echo 'manual method done'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 }
@@ -131,21 +131,21 @@ df
 #echo "sudo mv -v $RETP_FOLDER/* $MNTPNT"
 sudo mv -v $RETP_FOLDER/* $MNTPNT
 ls -l $MNTPNT
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo 'make symlinks' 
 #ln -s <destination file or directory> <name of the symlink> 
 ln -sT $MNTPNT $RETP_FOLDER
 
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 echo 'check if symlink is working'
 ls -l $RETP_FOLDER
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 echo 'symlink mehod done'
-read -rsp $'Press any key to continue...\n' -n 1 key
+read -rsp $'Press any key to continue...\n' -n 1
 # echo $key
 clear
 }
